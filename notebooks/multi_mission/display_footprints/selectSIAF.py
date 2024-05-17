308deca4de5701fa17fa11be917e7ed90e0b4d6b
Brian McLean <mclean@stsci.edu>
2024-05-17 12:14:39 -0400

    More PEP errors

--- notebooks/multi_mission/display_footprints/selectSIAF.py
+++ notebooks/multi_mission/display_footprints/selectSIAF.py
@@ -52,7 +52,7 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
         if selectedInstrument.lower() == 'fgs':
             telescopeSiaf = pysiaf.Siaf(selectedInstrument)
             if selectedAperture.lower() == 'all' :
-                apertureNames=['FGS1_FULL','FGS2_FULL']
+                apertureNames=['FGS1_FULL', 'FGS2_FULL']
                 V2Ref = +100.0
                 V3Ref = -700.0
             else:
@@ -64,7 +64,7 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
         elif selectedInstrument.lower() == 'miri':
             telescopeSiaf = pysiaf.Siaf(selectedInstrument)
             if selectedAperture.lower() == 'all' :
-                apertureNames=['MIRIM_FULL','MIRIM_MASKLYOT','MIRIM_MASK1550','MIRIM_MASK1140','MIRIM_MASK1065']
+                apertureNames=['MIRIM_FULL', 'MIRIM_MASKLYOT', 'MIRIM_MASK1550', 'MIRIM_MASK1140', 'MIRIM_MASK1065']
                 V2Ref = -425.0
                 V3Ref = -375.0
             else:
@@ -76,8 +76,8 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
         elif selectedInstrument.lower() == 'nircam':
             telescopeSiaf = pysiaf.Siaf(selectedInstrument)
             if selectedAperture.lower() == 'all' :
-                apertureNames=['NRCA1_FULL','NRCA1_FULL','NRCA3_FULL','NRCA4_FULL','NRCA5_FULL',
-                               'NRCB1_FULL','NRCB1_FULL','NRCB3_FULL','NRCB4_FULL','NRCB5_FULL']       
+                apertureNames = ['NRCA1_FULL', 'NRCA1_FULL', 'NRCA3_FULL', 'NRCA4_FULL', 'NRCA5_FULL',
+                                 'NRCB1_FULL', 'NRCB1_FULL', 'NRCB3_FULL', 'NRCB4_FULL', 'NRCB5_FULL']       
                 V2Ref =    0.0
                 V3Ref = -500.0
             else:
@@ -89,8 +89,8 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
         elif selectedInstrument.lower() == 'nirspec':
             telescopeSiaf = pysiaf.Siaf(selectedInstrument)
             if selectedAperture.lower() == 'all' :
-                apertureNames=['NRS_FULL_MSA1','NRS_FULL_MSA2','NRS_FULL_MSA3','NRS_FULL_MSA4',
-                               'NRS1_FULL','NRS2_FULL']
+                apertureNames = ['NRS_FULL_MSA1', 'NRS_FULL_MSA2', 'NRS_FULL_MSA3', 'NRS_FULL_MSA4',
+                                 'NRS1_FULL', 'NRS2_FULL']
                 V2Ref = +375.0
                 V3Ref = -425.0
             else:
@@ -102,7 +102,7 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
         elif selectedInstrument.lower() == 'niriss':
             telescopeSiaf = pysiaf.Siaf(selectedInstrument)
             if selectedAperture.lower() == 'all' :
-                apertureNames=['NIS_CEN','NIS_AMIFULL']
+                apertureNames = ['NIS_CEN', 'NIS_AMIFULL']
                 V2Ref = -300.0
                 V3Ref = -700.0
             else:
@@ -113,25 +113,25 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
                 apertureList.append(telescopeSiaf[name])
         elif selectedInstrument.lower() == 'all':
             telescopeSiaf = pysiaf.Siaf('FGS')
-            apertureNames = ['FGS1_FULL','FGS2_FULL']
+            apertureNames = ['FGS1_FULL', 'FGS2_FULL']
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
             telescopeSiaf = pysiaf.Siaf('MIRI')
-            apertureNames=['MIRIM_FULL','MIRIM_MASKLYOT','MIRIM_MASK1550','MIRIM_MASK1140','MIRIM_MASK1065']
+            apertureNames =[ 'MIRIM_FULL', 'MIRIM_MASKLYOT', 'MIRIM_MASK1550', 'MIRIM_MASK1140', 'MIRIM_MASK1065']
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
             telescopeSiaf = pysiaf.Siaf('NIRCAM')
-            apertureNames=['NRCA1_FULL','NRCA1_FULL','NRCA3_FULL','NRCA4_FULL','NRCA5_FULL',
-                           'NRCB1_FULL','NRCB1_FULL','NRCB3_FULL','NRCB4_FULL','NRCB5_FULL']       
+            apertureNames = ['NRCA1_FULL', 'NRCA1_FULL', 'NRCA3_FULL', 'NRCA4_FULL', 'NRCA5_FULL',
+                             'NRCB1_FULL', 'NRCB1_FULL', 'NRCB3_FULL', 'NRCB4_FULL', 'NRCB5_FULL']       
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
             telescopeSiaf = pysiaf.Siaf('NIRSPEC')
-            apertureNames=['NRS_FULL_MSA1','NRS_FULL_MSA2','NRS_FULL_MSA3','NRS_FULL_MSA4',
-                           'NRS1_FULL','NRS2_FULL']
+            apertureNames = ['NRS_FULL_MSA1', 'NRS_FULL_MSA2', 'NRS_FULL_MSA3', 'NRS_FULL_MSA4',
+                             'NRS1_FULL', 'NRS2_FULL']
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
             telescopeSiaf = pysiaf.Siaf('NIRISS')
-            apertureNames=['NIS_CEN','NIS_AMIFULL']
+            apertureNames = ['NIS_CEN', 'NIS_AMIFULL']
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
             V2Ref = 0.0
@@ -143,11 +143,11 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
         telescopeSiaf = pysiaf.Siaf(selectedTelescope)
         if selectedInstrument.lower() == 'acs':
             if selectedAperture.lower() == 'all' :
-                apertureNames=['JWFC1','JWFC2','JHRC','JSBC']
+                apertureNames = ['JWFC1', 'JWFC2', 'JHRC', 'JSBC']
                 V2Ref = telescopeSiaf.apertures['JWFCENTER'].V2Ref
                 V3Ref = telescopeSiaf.apertures['JWFCENTER'].V3Ref
             else:
-                apertureNames=[selectedAperture]    
+                apertureNames = [selectedAperture]    
                 V2Ref = telescopeSiaf.apertures[selectedAperture].V2Ref
                 V3Ref = telescopeSiaf.apertures[selectedAperture].V3Ref
             for name in apertureNames:
@@ -155,66 +155,66 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
 
         elif selectedInstrument.lower() == 'cos':
             if selectedAperture.lower() == 'all' :
-                apertureNames=['LFMAC','LNMAC']
+                apertureNames = ['LFMAC', 'LNMAC']
                 V2Ref = telescopeSiaf.apertures['LFMAC'].V2Ref
                 V3Ref = telescopeSiaf.apertures['LFMAC'].V3Ref
             else:
-                apertureNames=[selectedAperture]    
+                apertureNames = [selectedAperture]    
                 V2Ref = telescopeSiaf.apertures[selectedAperture].V2Ref
                 V3Ref = telescopeSiaf.apertures[selectedAperture].V3Ref
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
         elif selectedInstrument.lower() == 'fgs':
             if selectedAperture.lower() == 'all' :
-                apertureNames=['FGS1','FGS2','FGS3']
+                apertureNames = ['FGS1', 'FGS2', 'FGS3']
                 V2Ref = 0.0
                 V3Ref = 0.0
             else:
-                apertureNames=[selectedAperture]    
+                apertureNames = [selectedAperture]    
                 V2Ref = telescopeSiaf.apertures[selectedAperture].V2Ref
                 V3Ref = telescopeSiaf.apertures[selectedAperture].V3Ref
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
         elif selectedInstrument.lower() == 'nicmos':
             if selectedAperture.lower() == 'all' :
-                apertureNames=['NIC1','NIC2','NIC3']
+                apertureNames = ['NIC1', 'NIC2', 'NIC3']
                 V2Ref = telescopeSiaf.apertures['NIC1'].V2Ref
                 V3Ref = telescopeSiaf.apertures['NIC1'].V3Ref
             else:
-                apertureNames=[selectedAperture]    
+                apertureNames = [selectedAperture]    
                 V2Ref = telescopeSiaf.apertures[selectedAperture].V2Ref
                 V3Ref = telescopeSiaf.apertures[selectedAperture].V3Ref
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
         elif selectedInstrument.lower() == 'stis':
             if selectedAperture.lower() == 'all' :
-                apertureNames=['OVIS','ONUV','OFUV']
+                apertureNames = ['OVIS', 'ONUV', 'OFUV']
                 V2Ref = telescopeSiaf.apertures['OVIS'].V2Ref
                 V3Ref = telescopeSiaf.apertures['OVIS'].V3Ref
             else:
-                apertureNames=[selectedAperture]    
+                apertureNames = [selectedAperture]    
                 V2Ref = telescopeSiaf.apertures[selectedAperture].V2Ref
                 V3Ref = telescopeSiaf.apertures[selectedAperture].V3Ref
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
         elif selectedInstrument.lower() == 'wfc3':
             if selectedAperture.lower() == 'all' :
-                apertureNames=['IUVIS1','IUVIS2','IIR']
+                apertureNames = ['IUVIS1', 'IUVIS2', 'IIR']
                 V2Ref = telescopeSiaf.apertures['IIR'].V2Ref
                 V3Ref = telescopeSiaf.apertures['IIR'].V3Ref
             else:
-                apertureNames=[selectedAperture]    
+                apertureNames = [selectedAperture]    
                 V2Ref = telescopeSiaf.apertures[selectedAperture].V2Ref
                 V3Ref = telescopeSiaf.apertures[selectedAperture].V3Ref
             for name in apertureNames:
                 apertureList.append(telescopeSiaf[name])
         elif selectedInstrument.lower() == 'all':
-            apertureNames=['JWFC1','JWFC2','JHRC','JSBC',
-                            'LFMAC',
-                            'FGS1','FGS2','FGS3',
-                            'NIC1','NIC2','NIC3',
-                            'OVIS','ONUV','OFUV',
-                            'IUVIS1','IUVIS2','IIR']
+            apertureNames = ['JWFC1', 'JWFC2', 'JHRC', 'JSBC',
+                             'LFMAC',
+                             'FGS1', 'FGS2', 'FGS3',
+                             'NIC1', 'NIC2', 'NIC3',
+                             'OVIS', 'ONUV', 'OFUV',
+                             'IUVIS1', 'IUVIS2', 'IIR']
             V2Ref = 0.0
             V3Ref = 0.0
         else:
@@ -230,16 +230,16 @@ def defineApertures(selectedTelescope, selectedInstrument, selectedAperture):
 
 def getVertices(apertureSiaf):
     if (apertureSiaf.observatory == 'Roman' and apertureSiaf.AperShape == 'QUAD'):
-        xVertices = np.array([apertureSiaf.XIdlVert1,apertureSiaf.XIdlVert2,apertureSiaf.XIdlVert3,apertureSiaf.XIdlVert4])        
-        yVertices = np.array([apertureSiaf.YIdlVert1,apertureSiaf.YIdlVert2,apertureSiaf.YIdlVert3,apertureSiaf.YIdlVert4])        
+        xVertices = np.array([apertureSiaf.XIdlVert1, apertureSiaf.XIdlVert2, apertureSiaf.XIdlVert3, apertureSiaf.XIdlVert4])        
+        yVertices = np.array([apertureSiaf.YIdlVert1, apertureSiaf.YIdlVert2, apertureSiaf.YIdlVert3, apertureSiaf.YIdlVert4])        
        
     if (apertureSiaf.observatory == 'JWST' and apertureSiaf.AperShape == 'QUAD'):
-        xVertices = np.array([apertureSiaf.XIdlVert1,apertureSiaf.XIdlVert2,apertureSiaf.XIdlVert3,apertureSiaf.XIdlVert4])        
-        yVertices = np.array([apertureSiaf.YIdlVert1,apertureSiaf.YIdlVert2,apertureSiaf.YIdlVert3,apertureSiaf.YIdlVert4])        
+        xVertices = np.array([apertureSiaf.XIdlVert1, apertureSiaf.XIdlVert2, apertureSiaf.XIdlVert3, apertureSiaf.XIdlVert4])        
+        yVertices = np.array([apertureSiaf.YIdlVert1, apertureSiaf.YIdlVert2, apertureSiaf.YIdlVert3, apertureSiaf.YIdlVert4])        
 
     if (apertureSiaf.observatory == 'HST' and (apertureSiaf.AperShape == 'QUAD' or apertureSiaf.AperShape == 'RECT')):
-        xVertices = np.array([apertureSiaf.v1x,apertureSiaf.v2x,apertureSiaf.v3x,apertureSiaf.v4x])        
-        yVertices = np.array([apertureSiaf.v1y,apertureSiaf.v2y,apertureSiaf.v3y,apertureSiaf.v4y])
+        xVertices = np.array([apertureSiaf.v1x, apertureSiaf.v2x, apertureSiaf.v3x, apertureSiaf.v4x])        
+        yVertices = np.array([apertureSiaf.v1y, apertureSiaf.v2y, apertureSiaf.v3y, apertureSiaf.v4y])
     if (apertureSiaf.observatory == 'HST' and apertureSiaf.AperShape == 'CIRC'):
         xVertices = apertureSiaf.V2Ref       
         yVertices = apertureSiaf.V3Ref
@@ -254,13 +254,13 @@ def getVertices(apertureSiaf):
 
 def computeStcsFootprint(apertureSiaf, skyRa, skyDec):
     if (apertureSiaf.AperShape == 'QUAD' or apertureSiaf.AperShape == 'RECT'):     
-        apertureSregion='POLYGON ICRS {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} '.format(skyRa[0],skyDec[0],skyRa[1],skyDec[1],skyRa[2],skyDec[2],skyRa[3],skyDec[3])
+        apertureSregion = 'POLYGON ICRS {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} '.format(skyRa[0], skyDec[0], skyRa[1], skyDec[1], skyRa[2], skyDec[2], skyRa[3], skyDec[3])
     elif apertureSiaf.AperShape == 'CIRC':
         radius = apertureSiaf.maj/3600.0
-        apertureSregion='CIRCLE ICRS {} {} {} '.format(skyRa, skyDec, radius)
+        apertureSregion = 'CIRCLE ICRS {} {} {} '.format(skyRa, skyDec, radius)
     #elif apertureSiaf.AperShape == 'PICK':
         #radius = apertureSiaf.maj/3600.0
-        #apertureSregion='POLYGON ICRS {} {} {} '.format(skyRa, skyDec, radius)
+        #apertureSregion = 'POLYGON ICRS {} {} {} '.format(skyRa, skyDec, radius)
     else:
         print('Unsupported shape {}').format(apertureSiaf.AperShape)
 
@@ -270,7 +270,7 @@ def computeStcsFootprint(apertureSiaf, skyRa, skyDec):
 
 def computeRegionFootprint(apertureSiaf, skyRa, skyDec):
     if (apertureSiaf.AperShape == 'QUAD' or apertureSiaf.AperShape == 'RECT'):
-        vertices = SkyCoord([skyRa[0], skyRa[1],skyRa[2], skyRa[3]],[skyDec[0], skyDec[1], skyDec[2], skyDec[3]], unit='deg')
+        vertices = SkyCoord([skyRa[0], skyRa[1], skyRa[2], skyRa[3]], [skyDec[0], skyDec[1], skyDec[2], skyDec[3]], unit='deg')
         region_sky = PolygonSkyRegion(vertices=vertices)
     elif apertureSiaf.AperShape == 'CIRC':
         radius = apertureSiaf.maj/3600.0
